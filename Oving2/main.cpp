968ead8305cd8f070835efd1b8464a2ee6f8c28e
ofplarsen <ofplarsen@gmail.com>
2022-01-31 15:58:39 +0100

    Looked at some examples and understand better the problems. Didnt understand that I should only have 1 task, not a list...

--- Oving2/main.cpp
+++ Oving2/main.cpp
@@ -19,81 +19,114 @@ class Workers{
     list<function<void()>> tasks;
     mutex wait_mutex;
     condition_variable cv;
-    condition_variable cvThreads;
-    atomic<bool> running;
+    vector<thread> threads;
+    bool end;
     bool wait;
-    bool joinWait;
+
 
 
     public:
-    vector<thread> threads;
+
         explicit Workers(int numOfThreads) {
             this->numberOfThreads = numOfThreads;
         }
 
         int start(){
+            threads.clear();
 
             for (int i = 0; i < this->numberOfThreads; ++i) {
-                wait = true;
-                cout << threads.size() << endl;
+
+
                 threads.emplace_back([this] {
-                    while (true) {
+                    bool fin = false;
+                    while (!fin) {
                         function<void()> task;
                         {
-                            unique_lock<mutex> lock(wait_mutex);
-                            while (wait)
-                                cv.wait(lock); // Unlock wait_mutex and wait. // When awaken, wait_mutex is locked.
 
-                            if (!tasks.empty()) {
-                                task = *tasks.begin();
-                                tasks.pop_front();
-                            } else{
-                                break;
+                            if(tasks.empty()){
+                                unique_lock<mutex> lock(wait_mutex);
+                                while (wait)
+                                    cv.wait(lock); // Unlock wait_mutex and wait. // When awaken, wait_mutex is locked.
+
+                            }
+
+                            {
+                                unique_lock<mutex> lock(tasks_mutex);
+                                if (!tasks.empty()) {
+                                    task = *tasks.begin();
+                                    tasks.pop_front();
+                                } else {
+
+                                    {
+                                        unique_lock<mutex> lock(threads_mutex);
+                                        if(end){
+                                            fin = true;
+                                        }else{
+                                            unique_lock<mutex> lock(wait_mutex);
+                                            wait = true;
+                                            break;
+                                        }
+                                    }
+
+                                }
                             }
                         }
 
-                        //cout << " runs in thread " << this_thread::get_id << endl;
                         if (task) {
                             task();
+                            cv.notify_one();
                         }
 
                     }
                 });
 
-
-                //this_thread::sleep_for(1s);
-
-                {
-                    unique_lock<mutex> lock(wait_mutex);
-                    wait = false;
-                }
-
-                cv.notify_one(); // Awake waiting cv
-
             }
         }
 
-    void post(list<function<void()>> tasksToPost){
-        unique_lock<mutex> lock(tasks_mutex);
-        tasks.insert(tasks.end(), tasksToPost.begin(), tasksToPost.end());
-    }
+        void post(function<void()> taskToPost){
 
-    void post_timeout(list<function<void()>> tasksToPost, int ms){
+            {
+                unique_lock<mutex> lock(tasks_mutex);
+                tasks.emplace_back(taskToPost);
+            }
 
+            {
+                unique_lock<mutex> lock(wait_mutex);
+                wait = false;
+            }
 
-        thread t([this, &ms, &tasksToPost] {
-            this_thread::sleep_for(chrono::milliseconds(ms));
-            post(tasksToPost);
-        });
+            cv.notify_all();
+        }
 
-        t.join();
-        start();
-    }
+        void post_timeout(function<void()> taskToPost, int ms){
+            {
+                unique_lock<mutex> lock(tasks_mutex);
+                tasks.emplace_back([&ms, &taskToPost]{
+                    this_thread::sleep_for(chrono::milliseconds(ms));
+                    taskToPost();
+                });
+            }
 
-    void stop() {
+            cv.notify_all();
+        }
 
-        for (auto &thread : threads) thread.join();
-    }
+        void stop() {
+            {
+                unique_lock<mutex> lock(threads_mutex);
+                end = false;
+            }
+            {
+                unique_lock<mutex> lock(wait_mutex);
+                wait = false;
+            }
+            cv.notify_one();
+        }
+
+        void join(){
+
+            for (auto &thread : threads) thread.join();
+
+        }
 
 };
 
@@ -102,50 +135,31 @@ int main() {
     Workers worker_thread(4);
     Workers event_loop(1);
 
-    list<function<void()>> tasksA;
-    list<function<void()>> tasksB;
-    list<function<void()>> tasksC;
-    list<function<void()>> tasksD;
-
-    for (int i = 0; i < 10; ++i) {
-        tasksA.emplace_back([i]{
-            cout << "A " << i << " runs in thread " << endl;
-        });
-    }
-    for (int i = 0; i < 10; ++i) {
-        tasksB.emplace_back([i]{
-            cout << "B " << i << " runs in thread " << endl;
-        });
-    }
-
-    for (int i = 0; i < 10; ++i) {
-        tasksC.emplace_back([i]{
-            cout << "C " << i << " runs in thread " << endl;
-        });
-    }
-
-    for (int i = 0; i < 10; ++i) {
-        tasksD.emplace_back([i]{
-            cout << "D " << i << " runs in thread " << endl;
-        });
-    }
-
-    worker_thread.post(tasksA);
-    worker_thread.post_timeout(tasksA,1000);
-    worker_thread.post_timeout(tasksB,500);
-
-    event_loop.post(tasksC);
-    event_loop.post(tasksD);
-
 
     event_loop.start();
     worker_thread.start();
 
+    worker_thread.post_timeout([]{
+        cout << "Task A" << endl;
+    }, 2000);
+
+    worker_thread.post([]{
+        cout << "Task B" << endl;
+    });
+
+    event_loop.post([]{
+        cout << "Task C" << endl;
+    });
+
+    event_loop.post([]{
+        cout << "Task D" << endl;
+    });
 
 
     worker_thread.stop();
     event_loop.stop();
-
+    worker_thread.join();
+    event_loop.join();
     return 0;
 }
 
