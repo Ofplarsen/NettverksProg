0564a32bc63b93df5ae04d0f92c118be7c36976c
ofplarsen <ofplarsen@gmail.com>
2022-02-23 17:24:08 +0100

    Fixed bug where I couldnt see error message if error in code

--- Oving5/backend/backend_server.js
+++ Oving5/backend/backend_server.js
@@ -10,13 +10,21 @@ const fs = require('fs')
 app.post('/compile', (req, res) => {
     writeFile(req.body["code"])
     const { exec } = require("child_process")
-    exec("docker build \"./cpp/\" -t ubuntu_image",() => {
-        exec("docker run --rm ubuntu_image", (error, stdout, stderr) => {
-            console.log(stdout)
-            console.log(stderr)
-            res.status(200).send(JSON.stringify({result:stdout}))
-        })
+    exec("docker build \"./cpp/\" -t ubuntu_image",(err,stdout, stderr) => {
+
+        if(err){
+
+            console.log(getError(stdout))
+            res.status(200).send(JSON.stringify({result:getError(stdout)}))
+        }else {
+            exec("docker run --rm ubuntu_image", (error, stdout, stderr) => {
+                console.log(stdout)
+                console.log(stderr)
+                res.status(200).send(JSON.stringify({result: stdout}))
+            })
+        }
     })
+
 })
 
 const server = app.listen(8990, () => {
@@ -29,4 +37,21 @@ function writeFile(code){
             console.log(err)
         }
     })
+}
+
+function getError(string){
+    lines = string.split("\n")
+    found = false
+    result = ""
+    for (let i = 0; i < lines.length; i++){
+
+        if(found){
+            result += lines[i] + "\n"
+        }
+
+        if(lines[i].includes("Running in")){
+            found = true
+        }
+    }
+    return result
 }
